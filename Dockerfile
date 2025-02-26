ab3b0a4b7493320d36894652a6e5c92c48edf985
d.fomin <d.fomin@els24.com>
2025-02-26 17:16:15 +0700

    Road to php 8.4

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM php:8.3-fpm-alpine
+FROM php:8.4-fpm-alpine
 
 # Установка необходимых пакетов и инструментов
 RUN apk update \
--- app/app/App/Filters/FilterResource.php
+++ app/app/App/Filters/FilterResource.php
@@ -7,26 +7,13 @@ use Countable;
 final class FilterResource
 {
     public function __construct(
-        private int $total,
-        private Countable $collection,
+        public private(set) int $total,
+        public Countable $collection {
+            set(Countable $collection){
+                $this->collection = $collection;
+                $this->total = $this->total ?? count($collection);
+            }
+        }
     ) {
-    }
-
-    public function getTotal(): int
-    {
-        return $this->total;
-    }
-
-    public function getCollection(): mixed
-    {
-        return $this->collection;
-    }
-
-    public function setCollection(
-        Countable $collection,
-        int $total = null,
-    ): void {
-        $this->collection = $collection;
-        $this->total = $total ?? count($collection);
     }
 }
